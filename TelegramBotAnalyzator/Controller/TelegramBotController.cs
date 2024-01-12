442d4f20a629d3288695f7bcc6f05dbe9565a769
rusta <rusta@DESKTOP-P1D9ESL>
2024-01-12 22:46:02 +0300

    Добавьте файлы проекта.

---
+++ TelegramBotAnalyzator/Controller/TelegramBotController.cs
@@ -0,0 +1,51 @@
+﻿using PRTelegramBot.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelegramBotAnalyzatorBL.Controller
+{
+    public class TelegramBotController
+    {
+        PRBot telegramBot;
+
+        public TelegramBotController()
+        {
+            telegramBot = new PRBot(option =>
+            {
+                option.Token = "6704352372:AAE3HkkV4x4wwGmITJutehTF1AzAfEROfNk";
+                option.ClearUpdatesOnStart = true;
+                option.WhiteListUsers = new List<long>() { };
+                option.Admins = new List<long>() { };
+                option.BotId = 0;
+            });
+
+            telegramBot.OnLogCommon += Telegram_OnLogCommon;
+            telegramBot.OnLogError += Telegram_OnLogError;
+            Start();
+        }
+
+        private async void Start()
+        {
+            await telegramBot.Start();
+        }
+
+        private void Telegram_OnLogError(Exception ex, long? id)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            string errorMsg = $"{DateTime.Now}: {ex}";
+            Console.WriteLine(errorMsg);
+            Console.ResetColor();
+        }
+
+        private void Telegram_OnLogCommon(string msg, PRBot.TelegramEvents typeEvent, ConsoleColor color)
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            string message = $"{DateTime.Now}: {msg}";
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
---
+++ TelegramBotAnalyzatorGUI/Program.cs
@@ -0,0 +1,14 @@
+﻿using TelegramBotAnalyzatorBL.Controller;
+
+const string EXIT_COMMAND = "exit";
+
+TelegramBotController tgBotController = new TelegramBotController();
+
+while (true)
+{
+    var result = Console.ReadLine();
+    if(result.ToLower() == EXIT_COMMAND)
+    {
+        Environment.Exit(0);
+    }
+}
